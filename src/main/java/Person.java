a75f75770ca390dafb53191b785fa45356de5d7d
22mbaird <22mbaird@athenian.org>
2020-10-28 00:55:53 +0000

    Classwork

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,5 +1,43 @@
 public class Person {
     
+    public String firstname;
+    public String lastname;
+
+    public Person(String a, String b) {
+        firstname = a;
+        lastname = b;
+    }
+
+    public void doSomething(){
+        System.out.println("I am watching netflix");
+    }
+
+    public String toString()
+    {
+        return this.firstname + " " + this.lastname;
+    }
+    public String getFirstName(){
+        return this.firstname;
+    }
+     public String getLastName(){
+        return this.lastname;
+    }
+
+    public void setFirstName(String str){
+        if (str.trim().length()!=0){
+            this.firstname = str;
+        }
+    }
+    public void setLastName(String str){
+        if (str.trim().length()!=0){
+            this.lastname = str;
+
+        }
+    }
+
+
+
+    
 }
 
 
